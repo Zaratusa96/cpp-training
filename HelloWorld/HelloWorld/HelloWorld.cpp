5311d499a1afed83629aeb451d4144842a3d649f
Zaratusa96 <miguelduartecampos@gmail.com>
2022-03-09 22:59:34 +0000

    1.5 iostream

--- HelloWorld/HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld/HelloWorld.cpp
@@ -7,7 +7,15 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::cout << "Enter two numbers separated by a space: ";
+
+	int x{ }; // define variable x to hold user input (and zero-initialize it)
+	int y{ }; // define variable y to hold user input (and zero-initialize it)
+	std::cin >> x >> y; // get two numbers and store in variable x and y respectively
+
+	std::cout << "You entered " << x << " and " << y << '\n';
+
+	return 0;
 
 
 	// This code should be kept at the end of main function.
